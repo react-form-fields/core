a0042a205601ac55c9c3790bf70ad467bf4a9618
Daniel Prado <danielprado.ad@gmail.com>
2018-06-30 11:40:28 -0300

    initial

---
+++ declarations/extra.d.ts
@@ -0,0 +1,7 @@
+declare module 'draftjs-to-html';
+declare module 'html-to-draftjs';
+declare module 'draft-js-import-html';
+declare module '@material-ui/icons/Search';
+declare module '@material-ui/icons/Close';
+declare module '@material-ui/icons/ChevronLeft';
+declare module '@material-ui/icons/ChevronRight';
