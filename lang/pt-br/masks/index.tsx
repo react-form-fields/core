ebea587c01a1034d069bd6adc06ec7cab9f8dd8f
danieloprado <danielprado.ad@gmail.com>
2019-02-05 17:11:48 -0200

    add money mask
    Signed-off-by: danieloprado <danielprado.ad@gmail.com>

---
+++ lang/pt-br/masks/index.tsx
@@ -0,0 +1,17 @@
+import { IMask } from '../../../mask';
+import cnpj from './cnpj';
+import cpf from './cpf';
+import document from './document';
+import money from './money';
+import phone from './phone';
+import zipcode from './zipcode';
+
+const masks: IMask[] = [
+  { name: 'zipcode', ...zipcode },
+  { name: 'phone', ...phone },
+  { name: 'document', ...document },
+  { name: 'cpf', ...cpf },
+  { name: 'cnpj', ...cnpj },
+  { name: 'money', ...money }
+];
+export default masks;
